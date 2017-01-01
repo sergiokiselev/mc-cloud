b7e432fe233163b2f54c1ae57d903d8cf20c1541
seregakiselev <sammy_666@mail.ru>
2017-01-01 16:14:38 +0300

    Init

--- src/main/java/com/cloud/CloudApplication.java
+++ src/main/java/com/cloud/CloudApplication.java
@@ -1,18 +1,18 @@
-package com.cloud;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-
-/**
- * User: NotePad.by
- * Date: 12/31/2016.
- */
-@SpringBootApplication
-public class CloudApplication extends SpringBootServletInitializer {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CloudApplication.class, args);
-    }
-
-}
+//package com.cloud;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.context.web.SpringBootServletInitializer;
+//
+///**
+// * User: NotePad.by
+// * Date: 12/31/2016.
+// */
+//@SpringBootApplication
+//public class CloudApplication extends SpringBootServletInitializer {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(CloudApplication.class, args);
+//    }
+//
+//}
